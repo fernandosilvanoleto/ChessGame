a6834a53f551b48d26971268006cd0da6db88200
Fernando Silva Noleto <fernandonoleto17@gmail.com>
2019-12-10 13:46:52 -0300

    Tratando Exceções em Tabuleiro Chess.

--- ChessGame/Tabuleiro/TabuleiroChess.cs
+++ ChessGame/Tabuleiro/TabuleiroChess.cs
@@ -24,10 +24,41 @@ namespace ChessGame.Tabuleiro
             return pecas[linha, coluna];
         }
 
+        public Peca peca(Posicao pos)
+        {
+            return pecas[pos.Linha, pos.Coluna];
+        }
+
+        public bool existePeca(Posicao pos)
+        {
+            validarPosicao(pos);
+            return peca(pos) != null;
+        }
+
         public void colocarPeca(Peca p, Posicao pos)
         {
             pecas[pos.Linha, pos.Coluna] = p;
             p.posicao = pos;
+        }
+
+        public bool posicaoValida(Posicao pos)
+        {
+            if(pos.Linha < 0 || pos.Linha >= Linhas || pos.Coluna < 0 || pos.Coluna >= Colunas)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public void validarPosicao(Posicao pos)
+        {
+            if (!posicaoValida(pos))
+            {
+                throw new TabuleiroChessException("Posição Inválida!!!");
+            }
         }
         
     }
