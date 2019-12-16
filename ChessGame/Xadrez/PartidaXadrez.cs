c86e28eb9d06f5a71fb86744d55710286c8d4107
Fernando Silva Noleto <fernandonoleto17@gmail.com>
2019-12-16 14:19:13 -0300

    Tratamento de peças capturadas - Parte 01

--- ChessGame/Xadrez/PartidaXadrez.cs
+++ ChessGame/Xadrez/PartidaXadrez.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ChessGame.Tabuleiro;
+using System.Collections.Generic;
 
 namespace ChessGame.Xadrez
 {
@@ -13,6 +14,8 @@ namespace ChessGame.Xadrez
         public int turno { get; private set; }
         public Color jogadorAtual { get; private set; }
         public bool Terminada { get; private set; }
+        private HashSet<Peca> pecas;
+        private HashSet<Peca> capturadas;
 
         public PartidaXadrez()
         {
@@ -20,6 +23,8 @@ namespace ChessGame.Xadrez
             turno = 1;
             jogadorAtual = Color.Branca;
             Terminada = false;
+            pecas = new HashSet<Peca>();
+            capturadas = new HashSet<Peca>();
             colocarPecas();
         }
 
@@ -29,6 +34,10 @@ namespace ChessGame.Xadrez
             p.incrementarQtdeMovimento();
             Peca pecaCapturada = tab.retirarPeca(destino);
             tab.colocarPeca(p, destino);
+            if (pecaCapturada != null)
+            {
+                capturadas.Add(pecaCapturada);
+            }
         }
 
         public void realizaJogada(Posicao origem, Posicao destino)
@@ -74,9 +83,56 @@ namespace ChessGame.Xadrez
             }
         }
 
+        public HashSet<Peca> pecasCapturadas(Color cor)
+        {
+            HashSet<Peca> aux = new HashSet<Peca>();
+            foreach (Peca item in capturadas)
+            {
+                if (item.cor == cor)
+                {
+                    aux.Add(item);
+                }
+            }
+            return aux;
+        }
+
+        public HashSet<Peca> pecasEmJogo(Color cor)
+        {
+            HashSet<Peca> aux = new HashSet<Peca>();
+            foreach (Peca item in capturadas)
+            {
+                if (item.cor == cor)
+                {
+                    aux.Add(item);
+                }
+            }
+            aux.ExceptWith(pecasCapturadas(cor));
+            return aux;
+        }
+
+        public void colocarNovaPeca(char coluna, int linha, Peca peca)
+        {
+            tab.colocarPeca(peca, new PosicaoXadrez(coluna, linha).toPosicao());
+            pecas.Add(peca);
+        }
+
         private void colocarPecas()
         {
-            tab.colocarPeca(new Torre(tab, Color.Branca), new PosicaoXadrez('c', 1).toPosicao());
+            colocarNovaPeca('c', 1, new Torre(tab, Color.Branca));
+            colocarNovaPeca('c', 2, new Torre(tab, Color.Branca));
+            colocarNovaPeca('d', 2, new Torre(tab, Color.Branca));
+            colocarNovaPeca('e', 2, new Torre(tab, Color.Branca));
+            colocarNovaPeca('e', 1, new Torre(tab, Color.Branca));
+            colocarNovaPeca('d', 1, new Torre(tab, Color.Branca));
+
+            colocarNovaPeca('c', 7, new Torre(tab, Color.Preta));
+            colocarNovaPeca('c', 8, new Torre(tab, Color.Preta));
+            colocarNovaPeca('d', 7, new Torre(tab, Color.Preta));
+            colocarNovaPeca('e', 7, new Torre(tab, Color.Preta));
+            colocarNovaPeca('e', 8, new Torre(tab, Color.Preta));
+            colocarNovaPeca('d', 8, new Torre(tab, Color.Preta));
+
+            /*tab.colocarPeca(new Torre(tab, Color.Branca), new PosicaoXadrez('c', 1).toPosicao());
             tab.colocarPeca(new Torre(tab, Color.Branca), new PosicaoXadrez('c', 2).toPosicao());
             tab.colocarPeca(new Torre(tab, Color.Branca), new PosicaoXadrez('d', 2).toPosicao());
             tab.colocarPeca(new Torre(tab, Color.Branca), new PosicaoXadrez('e', 2).toPosicao());
@@ -89,7 +145,7 @@ namespace ChessGame.Xadrez
             tab.colocarPeca(new Torre(tab, Color.Preta), new PosicaoXadrez('e', 7).toPosicao());
             tab.colocarPeca(new Torre(tab, Color.Preta), new PosicaoXadrez('e', 8).toPosicao());
             tab.colocarPeca(new Rei(tab, Color.Preta), new PosicaoXadrez('d', 8).toPosicao());
-
+            */
         }
 
     }
